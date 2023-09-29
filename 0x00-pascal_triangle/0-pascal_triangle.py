81e235674837884b1cfe4c03498b7ccc9dd6f17d
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-29 05:58:34 +0100

    Finished the first task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This script, determines pascal's triangle for any number"""
+
+
+def pascal_triangle(n):
+    """
+    returns the list of lists of integers representing the Pascal’s triangle
+    of n
+    """
+    triangle = []
+
+    # return (trianlgle if n <= 0)
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
+    return triangle
